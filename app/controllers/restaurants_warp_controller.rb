0e27666512b0b82b8defca83ba34fc335773874f
jordan-laird <jordanmack.laird@gmail.com>
2019-01-16 19:34:54 -0600

    googleAPI key fixed figaro added.

--- app/controllers/restaurants_warp_controller.rb
+++ app/controllers/restaurants_warp_controller.rb
@@ -1,6 +1,7 @@
+
 class RestaurantsWarpController < ApplicationController
   def index(params)
-    response = JSON.parse(RestClient.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=29.759236,-95.3633486&key=AIzaSyDbgCD5xqCrFD1hLovVCrEN0NOSd8Vk_b0&radius=16093.4&type=restaurant&keyword="))
-    yield response
+    response = JSON.parse(RestClient.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=29.759236,-95.3633486&key=#{Figaro.env.googleKey}&radius=16093.4&type=restaurant&keyword="))
+    yield json: response
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,6 +23,8 @@ module Mod5Back
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_controller.default_protect_from_forgery = false
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
