9aef774c3c57fc1455da1a1a2df7e758cbc72653
jordan-laird <jordanmack.laird@gmail.com>
2019-01-21 22:12:42 -0600

    Changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
     has_one_attached :avatar 
     has_many :meals
     has_many :invites
+    has_many :users, through: :invites
+    has_many :messages
+    has_many :users, through: :messages
     # validations
 end
